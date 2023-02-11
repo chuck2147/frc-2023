0546d9f3926938093fcf55af35ccc4786a1f8efb
Kenton Bell <125155938+kentnbell@users.noreply.github.com>
2023-02-11 14:39:58 -0800

    auto build

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,6 +148,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new exampleAuto(s_Swerve);
+    return new exampleAuto(s_Swerve, intakeSubsystem);
   }
 }
--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -1,22 +1,27 @@
 package frc.robot.autos;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-// import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Swerve;
 
 public class exampleAuto extends ParallelCommandGroup {
-  public exampleAuto(Swerve s_Swerve) {
-    addRequirements(s_Swerve);
+ 
+  public exampleAuto(Swerve s_Swerve, IntakeSubsystem intakeSubsystem) {
+    addRequirements(s_Swerve, intakeSubsystem);
+    
     addCommands(
       new RunCommand(() -> s_Swerve.drive(new Translation2d(0.3, 0), 0, true, true )).withTimeout(3),
       new RunCommand(() -> s_Swerve.drive(new Translation2d(-0.3, 0), 0, true, true )).withTimeout(3)
     );
-    // addCommands(
-    //   new RunCommand(() -> IntakeSubsystem.
-    // );
-    //
+    
+    addCommands(
+      new RunCommand(() -> intakeSubsystem.forwardIntakeMotor()).withTimeout(3),
+      new RunCommand(() -> intakeSubsystem.stopIntakeMotor()).withTimeout(3)//,
+      // new InstantCommand(()->s_Swerve.resetOdometry(null))
+    );
+    
   }
 }
