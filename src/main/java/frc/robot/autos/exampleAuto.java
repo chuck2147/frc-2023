9dd9f2a31c15560fbd09882b8dde1c7774e6a053
Kenton Bell <125155938+kentnbell@users.noreply.github.com>
2023-02-22 16:56:37 -0800

    ++autoBalance timing

--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -3,24 +3,26 @@ package frc.robot.autos;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Swerve;
 
-public class exampleAuto extends ParallelCommandGroup {
+public class exampleAuto extends SequentialCommandGroup {
  
   public exampleAuto(Swerve s_Swerve, IntakeSubsystem intakeSubsystem) {
     addRequirements(s_Swerve, intakeSubsystem);
-    
+  
     addCommands(
-      new RunCommand(() -> s_Swerve.drive(new Translation2d(0.3, 0), 0, true, true )).withTimeout(3),
-      new RunCommand(() -> s_Swerve.drive(new Translation2d(-0.3, 0), 0, true, true )).withTimeout(3)
+      new RunCommand(() -> s_Swerve.drive(new Translation2d(1.7, 0), 0, true, true )).withTimeout(1.7),
+      new RunCommand(() -> s_Swerve.drive(new Translation2d(2.4, 0), 0, true, true )).withTimeout(1.25),
+      new RunCommand(() -> s_Swerve.drive(new Translation2d(-1.7, 0), 0, true, true )).withTimeout(4.7)
     );
     
-    addCommands(
-      new RunCommand(() -> intakeSubsystem.forwardIntakeMotor()).withTimeout(3),
-      new RunCommand(() -> intakeSubsystem.stopIntakeMotor()).withTimeout(3)//,
-      // new InstantCommand(()->s_Swerve.resetOdometry(null))
-    );
+    // addCommands(
+    //   new RunCommand(() -> intakeSubsystem.forwardIntakeMotor()).withTimeout(3),
+    //   new RunCommand(() -> intakeSubsystem.stopIntakeMotor()).withTimeout(3)//,
+    //   // new InstantCommand(()->s_Swerve.resetOdometry(null))
+    // );
     
   }
 }
